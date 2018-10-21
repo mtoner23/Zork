9844ebf8354bb9afe5c837527d7e1f96374ac00b
ybharatu <ybharatu@purdue.edu>
2018-10-21 14:22:07 -0400

    Zork Header File created

---
+++ include/Zork.h
@@ -0,0 +1,35 @@
+#ifndef ZORK_H_
+#define ZORK_H_
+
+#include "../include/rapidxml.hpp"
+#include <vector>
+#include <map>
+#include <iterator>
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <fstream>
+#include "../include/room.h"
+#include "../include/Item.h"
+#include "../include/Container.h"
+#include "../include/Creature.h"
+
+using namespace std;
+using namespace rapidxml;
+
+class Zork {
+public:
+	Zork(char* filename);
+	~Zork();
+	string usr_input = "";
+	string curr_room;
+	vector<Item*> inventory;
+	vector<Room*> rooms;
+	vector<Item*> items;
+	vector<Container*> containers;
+	vector<Creature*> creatures;
+private:
+	int setup(void);
+};
+
+#endif
